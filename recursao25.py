0e380fe4000f273229d5d655e9487b5c966e4c30
jaja-j <jamillyevelyn57@gmail.com>
2026-06-08 21:29:46 -0300

    feito

---
+++ recursao25.py
@@ -0,0 +1,19 @@
+int tetranacci(int n){
+
+    if(n == 0)
+        return 0;
+
+    if(n == 1)
+        return 0;
+
+    if(n == 2)
+        return 0;
+
+    if(n == 3)
+        return 1;
+
+    return tetranacci(n - 1)
+         + tetranacci(n - 2)
+         + tetranacci(n - 3)
+         + tetranacci(n - 4);
+}
